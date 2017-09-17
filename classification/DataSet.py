75cb435803f82c9605acb7c2c51da9e09efc04fd
sreejithsekhar <sekhar.sreejith@gmail.com>
2017-09-17 08:42:38 -0400

    Made dataset modular

---
+++ classification/DataSet.py
@@ -0,0 +1,14 @@
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def getIrisDataSet():
+    iris = datasets.load_iris()
+    X = iris.data[:, [2, 3]]
+    y = iris.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    sc = StandardScaler()
+    sc.fit(X_train)
+    X_train_std = sc.transform(X_train)
+    X_test_std = sc.transform(X_test)
+    return X_train_std, X_test_std, y_train, y_test
--- classification/PerceptronSci.py
+++ classification/PerceptronSci.py
@@ -1,21 +1,10 @@
-from sklearn import datasets
+import DataSet as ds
 import numpy as np
-from sklearn .cross_validation import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
 import Plotter
 
-iris = datasets.load_iris()
-X = iris.data[:, [2, 3]]
-y = iris.target
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
-
-sc = StandardScaler()
-sc.fit(X_train)
-X_train_std = sc.transform(X_train)
-X_test_std = sc.transform(X_test)
+X_train_std, X_test_std, y_train, y_test = ds.getIrisDataSet()
 
 ppn = Perceptron(n_iter= 40, eta0= 0.1, random_state= 0)
 ppn.fit(X_train_std, y_train)
